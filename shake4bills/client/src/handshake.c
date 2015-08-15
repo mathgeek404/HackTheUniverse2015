0e4a787a167ca3d7b7aabf6f9d62a55042ba0a4a
Sahit Mandala <mathgeek404@gmail.com>
2015-08-15 07:39:27 -0700

    shsake sahke sahek

--- shake4bills/client/src/handshake.c
+++ shake4bills/client/src/handshake.c
@@ -27,6 +27,7 @@ enum ShakeKeys {
 
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+    
 
 }
 
@@ -43,7 +44,77 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
 }
 
 
-void accel_data_handler(AccelData *data, uint32_t num_samples);
+void accel_data_handler(AccelData *data, uint32_t num_samples) {
+  // Process 10 events - every 1 second
+ 
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Handle");
+  int total_x = 0;
+  int total_y = 0;
+  int total_z = 0;
+  int avg_x = 0;
+  int avg_y = 0;
+  int avg_z = 0;
+    
+  int max_y = 0;
+  int min_y = 2000;
+    
+  for(uint32_t i = 0; i < num_samples; i++)
+    {
+      total_x += (int)data[i].x;
+      total_y += (int)data[i].y;
+      total_z += (int)data[i].z;
+      max_y = (max_y < (int)data[i].y ? (int)data[i].y : max_y);
+      min_y = (min_y > (int)data[i].y ? (int)data[i].y : min_y);
+    }
+ 
+  avg_x = total_x/(int)num_samples;
+  avg_y = total_y/(int)num_samples;
+  avg_z = total_z/(int)num_samples;
+ 
+  char *str_x="xxxxxxxxxx";
+  snprintf(str_x, sizeof(str_x), "%d", avg_x);
+  APP_LOG(APP_LOG_LEVEL_ERROR, str_x);
+ 
+  char *str_y="xxxxxxxxxx";
+  snprintf(str_y, sizeof(str_y), "%d", avg_y);
+  APP_LOG(APP_LOG_LEVEL_ERROR, str_y);
+ 
+  char *str_z="xxxxxxxxxx";
+  snprintf(str_z, sizeof(str_z), "%d", avg_z);
+  APP_LOG(APP_LOG_LEVEL_ERROR, str_z);
+ 
+  //if ((int)avg_y > 400)
+    if (max_y > 900)
+    {
+      //if ( (int)avg_x < 200 )
+        if (min_y < 200)
+	{
+	  //if ( (int) avg_z < 200)
+	   // {
+	      APP_LOG(APP_LOG_LEVEL_ERROR, "accel data detected!!");
+	      // Begin dictionary
+          uint8_t buffer[20];
+	      DictionaryIterator *iter;
+          app_message_outbox_begin(&iter);
+          APP_LOG(APP_LOG_LEVEL_ERROR, "Got Here");
+          dict_write_begin(iter, buffer, sizeof(buffer));
+          // Write the Data:
+          char* str = malloc(20);
+          snprintf(str, sizeof(str), "$%d.%02d", (int)payment_size, (int)(payment_size*100)%100);
+          dict_write_cstring(iter, 404, str);
+          // End:
+          const uint32_t final_size = dict_write_end(iter);
+          
+          APP_LOG(APP_LOG_LEVEL_ERROR, "Sending");
+	      // Send the message!
+	      app_message_outbox_send();
+	      
+	    }
+	}
+       
+    }
+}
+
 
 
 // Money WatchFace Window
@@ -85,7 +156,7 @@ static void select_click_payment_handler(ClickRecognizerRef recognizer, void *co
 static void down_click_payment_handler(ClickRecognizerRef recognizer, void *context) {
   payment_size -= 1.00;
   char *str = malloc(20);
-  snprintf(str, sizeof(str), "$%d.%02d", (int)payment_size, (int)(payment_size*100)%100);
+  snprintf(str, sizeof(str), "$%d.00", (int)payment_size); //, (int)(payment_size*100)%100
   text_layer_set_text(money_layer, str);
 }
 
@@ -118,6 +189,8 @@ static void init(void) {
   payment_size = 5.00;
  
   //createPopup();
+    
+    
 
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
@@ -125,72 +198,19 @@ static void init(void) {
   app_message_register_outbox_failed(outbox_failed_callback);
   app_message_register_outbox_sent(outbox_sent_callback);
  
-  accel_service_set_samples_per_update(25);
-  accel_data_service_subscribe(25, &accel_data_handler);
+  accel_service_set_samples_per_update(10);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+  accel_data_service_subscribe(10, accel_data_handler);
   //accel_tap_service_subscribe(&accel_tap_handler);
   //tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
-  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
- 
+    
   window_stack_push(window, animated);
 }
 
-void accel_data_handler(AccelData *data, uint32_t num_samples) {
-  // Process 10 events - every 1 second
- 
-  int total_x = 0;
-  int total_y = 0;
-  int total_z = 0;
-  int avg_x = 0;
-  int avg_y = 0;
-  int avg_z = 0;
-  for(uint32_t i = 0; i < num_samples; i++)
-    {
-      total_x += (int)data[i].x;
-      total_y += (int)data[i].y;
-      total_z += (int)data[i].z;
-    }
- 
-  avg_x = total_x/(int)num_samples;
-  avg_y = total_y/(int)num_samples;
-  avg_z = total_z/(int)num_samples;
- 
-  char *str_x="xxxxxxxxxx";
-  snprintf(str_x, sizeof(str_x), "%d", avg_x);
-  APP_LOG(APP_LOG_LEVEL_ERROR, str_x);
- 
-  char *str_y="xxxxxxxxxx";
-  snprintf(str_y, sizeof(str_y), "%d", avg_y);
-  APP_LOG(APP_LOG_LEVEL_ERROR, str_y);
- 
-  char *str_z="xxxxxxxxxx";
-  snprintf(str_z, sizeof(str_z), "%d", avg_z);
-  APP_LOG(APP_LOG_LEVEL_ERROR, str_z);
- 
-  if ((int)avg_y > 400)
-    {
-      if ( (int)avg_x < 200 )
-	{
-	  if ( (int) avg_z < 200)
-	    {
-	      APP_LOG(APP_LOG_LEVEL_ERROR, "accel data detected!!");
-	      // Begin dictionary
-	      DictionaryIterator *iter;
-	      app_message_outbox_begin(&iter);
-
-	      // Add a key-value pair
-	      dict_write_uint8(iter, 0, 0);
-
-	      // Send the message!
-	      app_message_outbox_send();
-	      
-	    }
-	}
-       
-    }
-}
 
 static void deinit(void) {
   window_destroy(window);
+  accel_data_service_unsubscribe();
 }
 
 
